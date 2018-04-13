1bdd13b401aead978a8a260a729ac82045c3181f
ijohnnyh <ijohnnyh@live.unc.edu>
2018-04-13 17:30:48 -0400

    Run Length Encoder in Python

--- RunLengthEncoder.py
+++ RunLengthEncoder.py
@@ -7,9 +7,10 @@ class RunLength:
 		self.num_frames = 150
 
 	def encode(self, base):
-			input_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p.yuv', 'rb')
-			output_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-encoded.dat', 'w+b')
-			for frame in range(0, self.num_frames):
+		input_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p.yuv', 'rb')
+		output_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-encoded.dat', 'w+b')
+
+		for frame in range(0, self.num_frames):
 				print ('ENCODING FRAME {0}'.format(frame))
 				prev_symbol = input_stream.read(1)
 				occur = 1
@@ -22,29 +23,24 @@ class RunLength:
 						prev_symbol = curr_symbol
 						occur = 1
 					elif prev_symbol == curr_symbol:
-						occur+=1
 						if (occur == 255):
 							output_stream.write(curr_symbol)
 							output_stream.write(bytes([occur]))
-							occur = 0
-			input_stream.close()
-			output_stream.close()
-	
+							output_stream.flush()
+							occur = 1
+						else:
+							occur+=1
+		input_stream.close()
+		output_stream.close()
+
 	def decode(self, base):
-			input_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-encoded.dat', 'rb')
-			output_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-decoded.dat', 'w+b')
-
-			for frame in range(0, self.num_frames):
-				print ('DECODING FRAME {0}'.format(frame))
-				for pixels in range(0, self.width*self.height):
-					symbol = input_stream.read(1)
-					occur = int.from_bytes(input_stream.read(1), byteorder='big')
-					for i in range(0, occur):
-							output_stream.write(symbol)
-					output_stream.flush()
-			input_stream.close()
-			output_stream.close()
-
-
-
-
+		input_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-encoded.dat', 'rb')
+		output_stream = open('/Users/ijohnnyh/Documents/School/Classwork/COMP590/A1VideoFiles/'+base+'/'+base+'.450p-decoded.dat', 'w+b')
+		for frame in range(0, self.num_frames):
+			print ('DECODING FRAME {0}'.format(frame))
+			for pixels in range(0, self.width*self.height):
+				symbol = input_stream.read(1)
+				occur = int.from_bytes(input_stream.read(1), byteorder='big')
+				for i in range(0, occur):
+					output_stream.write(symbol)
+				output_stream.flush()
